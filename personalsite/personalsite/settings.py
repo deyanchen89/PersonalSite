ee29cebcc0fc9b56350b2db50166cb17f2b4e8b7
Deyan Chen <deyan.chen@tcl.com>
2021-05-12 15:45:35 +0800

    version 1.0

--- personalsite/personalsite/settings.py
+++ personalsite/personalsite/settings.py
@@ -118,10 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static')
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    ('css', os.path.join(STATIC_ROOT, 'css').replace('\\', '/')),
+    ('js', os.path.join(STATIC_ROOT, 'js').replace('\\', '/')),
+    ('images', os.path.join(STATIC_ROOT, 'images').replace('\\', '/')),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- personalsite/personalsite/views.py
+++ personalsite/personalsite/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def personalsite(request):
-	name = '陈德彦'
-	return render(request, 'index.html', {'name': name})
+	personal_info = {'name': 'Deyan Chen',
+					 'phone_num': '+(86) 155-210-44810',
+					 'email': 'deyanchen89@gmail.com',
+					 'address': 'California'}
+	return render(request, 'index.html', personal_info)
